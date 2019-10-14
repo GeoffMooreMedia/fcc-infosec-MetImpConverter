270bfe9ae73847ab2a84debdfe65db998f2d7cc0
Glitch (positive-helicopter) <none>
2019-10-14 02:09:58 +0000

    💅🎹 Checkpoint ./server.js:706117/98

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var cors        = require('cors');
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
+const helmet = require('helmet');
 
 var app = express();
 
@@ -17,6 +18,8 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+//prevent MIME type sniffing
+app.use(helmet.noSniff());
 
 //Index page (static HTML)
 app.route('/')
